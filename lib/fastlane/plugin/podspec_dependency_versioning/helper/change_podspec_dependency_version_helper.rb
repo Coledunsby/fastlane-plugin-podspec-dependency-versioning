f1cee069706513293ea8fffad5bc99d1d4babe01
Cole Dunsby <coledunsby@gmail.com>
2018-07-04 19:31:30 -0400

    cleanup

--- lib/fastlane/plugin/podspec_dependency_versioning/helper/change_podspec_dependency_version_helper.rb
+++ lib/fastlane/plugin/podspec_dependency_versioning/helper/change_podspec_dependency_version_helper.rb
@@ -1,16 +1,11 @@
 require 'fastlane_core/ui/ui'
 
 module Fastlane
-  UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
+    UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
 
-  module Helper
-    class ChangePodspecDependencyVersionHelper
-      # class methods that you define here become available in your action
-      # as `Helper::PodspecDependencyVersioningHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the podspec_dependency_versioning plugin helper!")
-      end
+    module Helper
+        class ChangePodspecDependencyVersionHelper
+            
+        end
     end
-  end
 end
